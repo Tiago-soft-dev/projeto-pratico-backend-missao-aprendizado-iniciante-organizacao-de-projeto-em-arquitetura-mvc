a65c6ace8aa4a38f9db03dcb2dbe5d04438b4731
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-25 11:50:17 -0300

    inclusão de validações

--- src/produto/produto.controller.js
+++ src/produto/produto.controller.js
@@ -12,13 +12,18 @@ async function readAll(req,res){
 
 async function readById(req,res){
     const id = req.params.id
-    console.log(id);
     const produto = await produtoService.readById(id)
+        if(!produto){
+            return res.send('produto não encontrado')
+        }
     res.send(produto)
 }
 
 async function create(req,res){
     const novoProduto = req.body
+        if(!novoProduto){
+            return res.send('corpo da requisição inválido')
+        }
     await produtoService.create(novoProduto)
     res.send(novoProduto)
 }
@@ -26,12 +31,18 @@ async function create(req,res){
 async function update(req,res){
     const id = req.params.id
     const update = req.body
+        if(!id || !update){
+            return res.send('dados informados inválidos')
+        }
     await produtoService.update(id, update)
     res.send(update)
 }
 
 async function deleteById(req,res){
     const id = req.params.id
+        if(!id){
+            return res.send('produto não encontrado')
+        }
     await produtoService.deleteById(id)
     res.send('produto deletado com sucesso')
 }
